4f43b21e3f3520c568762d8df0d2ede609247f14
syamn <admin@sakura-server.net>
2012-10-28 15:42:46 +0900

    Initial commit. Abstract structures.

---
+++ src/main/java/syam/witherblocker/ConfigurationManager.java
@@ -0,0 +1,13 @@
+/**
+ * WitherBlocker - Package: syam.witherblocker
+ * Created: 2012/10/28 15:41:33
+ */
+package syam.witherblocker;
+
+/**
+ * ConfigurationManager (ConfigurationManager.java)
+ * @author syam(syamn)
+ */
+public class ConfigurationManager {
+
+}
---
+++ src/main/java/syam/witherblocker/WitherBlocker.java
@@ -0,0 +1,13 @@
+/**
+ * WitherBlocker - Package: syam.witherblocker
+ * Created: 2012/10/28 15:40:22
+ */
+package syam.witherblocker;
+
+/**
+ * WitherBlocker (WitherBlocker.java)
+ * @author syam(syamn)
+ */
+public class WitherBlocker {
+
+}
